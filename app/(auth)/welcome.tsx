7ea59e0a639be08428d67ce168af2b60ad5f5419
zahidkhanmehsud <zahidkhanmsd2@gmail.com>
2025-01-09 01:26:24 +0500

    Implementing required dependecies (NativeWind , EsLint and Prettier) and restucture the project

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Welcome = () => {
+  return (
+    <SafeAreaView>
+      <Text>OnBoarding Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Welcome;
